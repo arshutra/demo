e444500bfbb88d43b25515bc9f223689ba3f9ba0
Arshiya Tabassum <120415170+arshutra@users.noreply.github.com>
2023-12-14 17:19:49 +0530

    Add files via upload

---
+++ shocase.js
@@ -0,0 +1,57 @@
+// cursore
+var cursor = document.querySelector('.cursor');
+var cursorinner = document.querySelector('.cursor2');
+var a = document.querySelectorAll('a');
+
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+});
+
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursorinner.style.left = x + 'px';
+  cursorinner.style.top = y + 'px';
+});
+
+document.addEventListener('mousedown', function(){
+  cursor.classList.add('click');
+  cursorinner.classList.add('cursorinnerhover')
+});
+
+document.addEventListener('mouseup', function(){
+  cursor.classList.remove('click')
+  cursorinner.classList.remove('cursorinnerhover')
+});
+
+a.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    cursor.classList.add('hover');
+  });
+  item.addEventListener('mouseleave', () => {
+    cursor.classList.remove('hover');
+  });
+})
+
+
+
+// ham
+
+function openmenu(){
+    document.getElementById('mobile-nav').classList.toggle('active')
+}
+
+function closemenu(){
+    document.getElementById('mobile-nav').classList.remove('active')
+}
+
+function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+}
+
+
+
+
